f854369290952e89bb7d03b5d1c314ef1f7d733b
firebreather-heart <dtenny95@gmail.com>
2023-11-07 05:59:04 +0100

    working

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -6,18 +6,19 @@ import requests
 
 # Create your views here.
 
-URL = "https://moviesdatabase.p.rapidapi.com/titles/"
+URL = "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming"
 
 HEADERS = {
 	"X-RapidAPI-Key": "fec099dd21mshcec4df7f75d81a3p1417ecjsn6aeee24e65bb",
 	"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
 }
+qs = {'startyear':2000}
 
 @swagger_auto_schema(method='GET',
 operation_description='GET /all\n Returns all movies')
 @api_view(['GET'])
 def get_all_movies(request, page=1):
-    res = requests.get(url=URL+f'?page={page}', headers=HEADERS)
+    res = requests.get(url=URL+f'?page={page}', headers=HEADERS, params=qs)
     res_json = res.json()
     return JsonResponse(res_json)
     
